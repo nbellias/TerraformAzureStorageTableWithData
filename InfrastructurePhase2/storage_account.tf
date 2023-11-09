3566f3e546ff4936bb0cfca0bb6343e9e074aca2
Nikolaos Bellias <nikolaos.bellias@zuehlke.com>
2023-11-09 10:45:34 +0100

    upd

--- InfrastructurePhase2/storage_account.tf
+++ InfrastructurePhase2/storage_account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "nikobstoracc"
+  name                     = "nikobteststoracc"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
